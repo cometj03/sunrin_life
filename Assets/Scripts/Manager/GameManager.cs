c0180c0323745a65d20c39d0d7c37b6eb93e69c7
XxCtrlZxX <harryjung1001@gmail.com>
2020-02-25 17:16:58 +0900

    TODO: write StageManager.cs

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int level;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        
+        //testText = GameObject.Find("testText").GetComponent<Text>();
+
+        //testText.text = "level:" + level.ToString();
+    }
+}
