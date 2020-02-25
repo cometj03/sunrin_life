e3ca0987f99c3be4696be04a2ec3ef211b1163b7
XxCtrlZxX <harryjung1001@gmail.com>
2020-02-25 22:57:58 +0900

    ADD : Particle System(Skill_A)

--- Assets/Scripts/Skills/Player_Skill_A.cs
+++ Assets/Scripts/Skills/Player_Skill_A.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Player_Skill_A : MonoBehaviour
 {
-    private float speed;
+    public GameObject Particle_A;
+    GameObject Player;
+    private float speed, angle;
 
     void Awake()
     {
+        if (Player == null)
+            Player = GameObject.Find("Player");
         speed = 12f;
+        angle = 0f;
     }
 
     void Update()
     {
         transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        // Arrow_angle 값 받아오기
+        angle = Player.GetComponent<ArrowRotation>().Arrow_angle;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,6 +37,9 @@ public class Player_Skill_A : MonoBehaviour
         else if (collision.tag == "Enemy")
         {
             GameObject.Find("HP Gauge").GetComponent<HPManager>().PlusHP(16f);
+
+            var clone = Instantiate(Particle_A, transform.position, Quaternion.Euler(0, 0, angle + 75f));
+            Destroy(clone, 1.0f);
             Destroy(gameObject);
         }
     }
